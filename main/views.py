3d27b5c2989e172e6aebc1d9f8d6f28749e4d728
anddp12 <anddp12@gmail.com>
2023-03-02 17:05:18 +0200

    Added form, database table, admin panel

--- main/views.py
+++ main/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import SubscriberForm
 
 
 def index(request):
-    # return HttpResponse("<h1>HOME</h1>")
-    return render(request, 'index.html')
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(data["name"])
+
+        new_form = form.save()
+    return render(request, 'index.html', locals())
 
 
 def resume(request):
